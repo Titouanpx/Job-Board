601120facaa2cc21a9ab0f8f028f0e721fa01590
Titouanpx <titouan.proux@epitech.eu>
2020-10-15 14:59:42 +0200

    added basic CRUD features

--- Step04/project/Controller.py
+++ Step04/project/Controller.py
@@ -10,29 +10,29 @@ def get_mdp_db():
 
 def connect():
     try:
-        connexion = mariadb.connect(
+        connection = mariadb.connect(
             user="root",
             password=get_mdp_db(),
             host="127.0.0.1",
             port=3306,
             database="jobboarddb"
         )
-        return connexion
+        return connection
     except mariadb.Error as e:
         print(f"Error connecting to MariaDB Platform: {e}")
         sys.exit(1)
 
 
-cursor = connect().cursor()
+connect = connect()
+cursor = connect.cursor()
 
 
-def create_new_ad(title, descr, wage, place, work_time, idpeople, idcompany):
-    cursor.execute("INSERT INTO advertisements (title, description, wage, place, working_time, id_people, id_company)"
-                   "VALUES (%s, %s, %s, %s, %s, %s, %s)", (title, descr, wage, place, work_time, idpeople, idcompany))
+def create_new_ad(id, title, descr, wage, place, work_time, idpeople, idcompany):
+    cursor.execute("INSERT INTO advertisements (id, title, description, wage, place, working_time, id_people, id_company)"
+                   "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (id, title, descr, wage, place, work_time, idpeople, idcompany))
+    connect.commit()
 
-    #cursor.commit()
-
-    print("record created")
+    print("ad created")
 
 
 def get_ad():
@@ -71,20 +71,18 @@ def get_ad_by_id_foreign_keys(ad_id):
     return data
 
 
-def update_ad(title, descr, wage, place, work_time, idpeople, idcompany):
-    cursor.execute("UPDATE advertisements "
-                   "Set title =" + title + ", description =" + descr + ", wage =" + wage + ", place =" + place +
-                   ", working_time =" + work_time + ", id_people =" + idpeople + ", id_company =" + idcompany)
+def update_ad(id, title, descr, wage, place, work_time, idpeople, idcompany):
+    cursor.execute('UPDATE advertisements '
+                   'SET title = "' + title + '", description = "' + descr + '", wage = "' + wage + '", place = "' +
+                   place + '", working_time = "' + work_time + '" , id_people = "' + idpeople + '", id_company ="' +
+                   idcompany + '" WHERE id = ' + id)
+    connect.commit()
 
-    #cursor.commit()
-
-    print("record updated")
+    print("ad updated")
 
 
 def delete_ad(id):
-    cursor.execute("DELETE FROM advertisements WHERE id = %s", id,)
+    cursor.execute('DELETE FROM advertisements WHERE id = ' + str(id))
+    connect.commit()
 
-    # cursor.commit()
-
-    print("record deleted")
-
+    print("ad deleted")
--- Step04/project/app.py
+++ Step04/project/app.py
@@ -1,7 +1,6 @@
 from Controller import *
 from flask import *
 
-
 app = Flask(__name__)
 
 
@@ -17,6 +16,37 @@ def learnmore(ad_id):
     data = get_ad_by_id_foreign_keys(ad_id)
 
     return render_template('learnMore.html', row=data)
+
+
+@app.route('/create')
+def create():
+    create_new_ad("5000", "title", "description", "1500", "place", "20", "1", "2")
+
+    data = get_ad_by_id(5000)
+
+    return render_template("learnMore.html", row=data)
+
+
+@app.route('/read')
+def read():
+    data = get_ad_by_id_foreign_keys(1)
+
+    return render_template("learnMore.html", row=data)
+
+
+@app.route('/update')
+def update():
+    update_ad("5000", "titleupdate", "descriptionupdate", "1500", "placeupdate", "20", "1", "2")
+    data = get_ad_by_id_foreign_keys(5000)
+
+    return render_template("learnMore.html", row=data)
+
+
+@app.route('/delete')
+def delete():
+    delete_ad(5000)
+
+    return "ad deleted"
 
 
 if __name__ == '__main__':
